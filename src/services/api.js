08b223e1f49a9b22fcd57c378a37ef8b96e26745
Alex Madeira <>
2018-09-25 09:58:44 -0300

    server

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:3333/"
-      : "http://ec2-54-233-83-34.sa-east-1.compute.amazonaws.com:3333/"
+      : "http://alexmadeira.com.br:3333/"
 });
 
 export default api;
