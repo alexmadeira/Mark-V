58144114ebf1eba00807d7dfb0b76b73d3659a40
Alex Madeira <>
2018-09-25 13:31:07 -0300

    banner Simples

--- src/components/banner/style.js
+++ src/components/banner/style.js
@@ -4,3 +4,9 @@ export const Container = styled.div`
   display: flex;
   background: #a1d7f0;
 `;
+
+export const Link = styled.a`
+  display: block;
+  height: 100%;
+  width: 100%;
+`;
--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://127.0.0.1:3333/"
+      ? "http://alexmadeira.com.br:3333/"
       : "http://alexmadeira.com.br:3333/"
 });
 
