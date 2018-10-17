486ea471f0aa27e646d61769b54bf1e7accb4847
Alex Madeira <alex.madeira@atomocom.com.br>
2018-10-17 11:02:23 -0300

    ajuste api

--- src/services/api.js
+++ src/services/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-      ? "http://alexmadeira.com.br:3333/"
-      : "http://alexmadeira.com.br:3333/"
+      ? "http://localhost:3333"
+      : "http://data.alexmadeira.com.br/"
 });
 
 export default api;
