4d174e0ea99031f6000c6c6315ed51f0b7da6459
iMac20 <imac20@iMac20.local>
2019-02-13 18:00:06 -0200

    banner:)

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,19 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// import Banner from "../../components/banner";
-import Projects from "../../components/lists/projetcs";
-import Basic from "../../components/block/basic";
-import Buttom from "../../components/buttons";
+import Banner from '../../components/banner';
+import Projects from '../../components/lists/projetcs';
+import Basic from '../../components/block/basic';
+import Buttom from '../../components/buttons';
 
-import "../../styles/global";
-import { Main, Inclina, Center, A, FigureBox } from "./style";
+import '../../styles/global';
+import {
+  Main, Inclina, Center, A, FigureBox,
+} from './style';
 
 const Home = ({ history }) => (
   <Main>
+    <Banner />
     <Inclina>
       <Projects limit={6} order="asc" history={history} simple />
       <Basic reverse shadow={false}>
@@ -20,20 +23,14 @@ const Home = ({ history }) => (
           <Buttom to="/projetos" history={history} type="outline">
             Projetos
           </Buttom>
-          <A
-            href="https://trampos.co/alexmadeira"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <A href="https://trampos.co/alexmadeira" target="_blank" rel="noopener noreferrer">
             Curriculo
           </A>
         </Center>
       </Basic>
     </Inclina>
     <Basic regular>
-      <h2 className="title">
-        Layout responsivo, fluido, mobile e todas as variacões possíveis
-      </h2>
+      <h2 className="title">Layout responsivo, fluido, mobile e todas as variacões possíveis</h2>
       <FigureBox>
         <img
           src="https://s3-sa-east-1.amazonaws.com/alex-madeira/imagens/geral/Responsive.jpg"
@@ -45,11 +42,7 @@ const Home = ({ history }) => (
       <h3>Gostou do que viu?</h3>
       <p>Entre em contato, ou não</p>
       <div>
-        <A
-          href="mailto:alex@alexmadeira.com.br"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <A href="mailto:alex@alexmadeira.com.br" target="_blank" rel="noopener noreferrer">
           Contato
         </A>
       </div>
@@ -58,7 +51,7 @@ const Home = ({ history }) => (
 );
 
 Home.propTypes = {
-  history: PropTypes.shape().isRequired
+  history: PropTypes.shape().isRequired,
 };
 
 export default Home;
